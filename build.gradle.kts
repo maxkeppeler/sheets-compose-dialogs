6775d6ff658022e967ca51372b6635bb88cf5ef1
Maximilian Keppeler <maximilian.keppeler@gmail.com>
2022-09-14 17:39:36 +0200

    Add spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application") version ("7.2.2") apply false
     id("com.android.library") version ("7.2.2") apply false
     id("org.jetbrains.kotlin.android") version ("1.7.0") apply false
+    id("com.diffplug.spotless") version ("6.10.0")
 }
 
 allprojects {
@@ -11,6 +12,17 @@ allprojects {
     }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+subprojects {
+    plugins.apply("com.diffplug.spotless")
+    spotless {
+        kotlin {
+            target("**/*.kt")
+//            ktlint(Versions.KT_LINT)
+            licenseHeaderFile(rootProject.file("copyright.kt"))
+        }
+        kotlinGradle {
+            target("*.gradle.kts", "gradle/*.gradle.kts", "buildSrc/*.gradle.kts")
+            licenseHeaderFile(rootProject.file("copyright.kt"), "import|tasks|apply|plugins|rootProject")
+        }
+    }
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,6 +3,8 @@ object Versions {
     const val KOTLIN = "1.7.0"
     const val COMPOSE = "1.2.0"
 
+    const val KT_LINT = "0.47.1"
+
     // Test
 
     const val JUNIT = "4.13.2"
