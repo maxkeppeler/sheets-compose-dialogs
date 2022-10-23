8ab81c1c94d7cddb48b08e86c437c5fda705d189
Maximilian Keppeler <maximilian.keppeler@gmail.com>
2022-10-23 17:17:57 +0200

    (General) Change min SDK

--- buildSrc/src/main/java/App.kt
+++ buildSrc/src/main/java/App.kt
@@ -3,7 +3,7 @@ object App {
     const val ID = "com.mk.sheets.compose"
 
     const val COMPILE_SDK = 33
-    const val MIN_SDK = 27
+    const val MIN_SDK = 21
     const val TARGET_SDK = 33
 
     const val VERSION_CODE = 1
