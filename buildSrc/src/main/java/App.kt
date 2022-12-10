435e1df70fa96e212e7601c12f367b9e3954b711
Maximilian Keppeler <maximilian.keppeler@gmail.com>
2022-12-10 20:27:51 +0100

    (General) Bump version to 1.0.2

--- buildSrc/src/main/java/App.kt
+++ buildSrc/src/main/java/App.kt
@@ -6,8 +6,8 @@ object App {
     const val MIN_SDK = 21
     const val TARGET_SDK = 33
 
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0.1"
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "1.0.2"
 
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 }
